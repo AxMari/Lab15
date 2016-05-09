5731b1f2a5f5c3e5dc829cf8e073b450de063d38
AxMari <AlexisMarien@gmail.com>
2016-05-09 19:17:28 -0400

    pre styling

---
+++ styles.js
@@ -0,0 +1,90 @@
+var app = angular.module('myModule', []); 
+app.controller('myController', function($scope, $interval){ 
+
+	function words(){
+	 	var word = ['what?\xA0', 'NO!\xA0', 'mine!\xA0', 'waaaaahhhhh!!!\xA0', 'gimme!\xA0'];
+	 	return word[Math.floor(Math.random() * word.length)];
+	};
+
+	function cssClass(){
+	 	var classes = [
+	 		'red',
+	 		'blue',
+	 		'green',
+	 		'big',
+	 		'small',
+	 		'arial', 
+	 		'courier', 
+	 		'bold',
+	 		'yellow',
+	 		'aquamarine'
+	 	];
+	 	return classes[Math.floor(Math.random() * classes.length)];
+	};
+	
+	function phrase(){
+	 	var phrases = ['no fair!\xA0', 'thats mine!\xA0', 'shes hitting me!\xA0', 'Im hungry!\xA0', 'I dont wanna nap\xA0' ];
+	 	return phrases[Math.floor(Math.random() * phrases.length)];
+	};
+	function wordButton(){
+		var item = {
+			text: words(),
+			style1: "",
+			style2: ""
+		};
+		return item;
+	};
+	function wordFancyButton(){
+		var item = {
+			text: words(),
+			style1: cssClass(),
+			style2: cssClass()
+		};
+		return item;
+	};
+	function phraseButton(){
+		var item = {
+			text: phrase(),
+			style1: "",
+			style2: ""
+		};
+		return item;
+	};
+	function phraseFancyButton(){
+		var item = {
+			text: phrase(),
+			style1: cssClass(),
+			style2: cssClass()
+		};
+		return item;
+	};
+	function functions(){
+		var funct = [
+		wordButton(),
+		wordFancyButton(),
+		phraseButton(),
+		phraseFancyButton()
+		];
+		return funct[Math.floor(Math.random() * funct.length)];
+	};
+
+
+	$scope.arr = [];
+
+	$scope.addWord= function(){
+		$scope.arr.push(wordButton());
+	};
+	$scope.addPhrase= function(){
+		$scope.arr.push(phraseButton());
+	};
+	$scope.addFancyStyle= function(){
+		$scope.arr.push(wordFancyButton());
+	};
+	$scope.addFancyPhrase= function(){
+		$scope.arr.push(phraseFancyButton());
+	};
+	$scope.bigButton= function(){	
+		$scope.arr.push(functions());
+	};
+
+});
