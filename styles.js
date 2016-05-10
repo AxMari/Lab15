cbc65f5f9325a0c9fcf4e74b8f32723b661e6fa4
AxMari <AlexisMarien@gmail.com>
2016-05-10 09:45:44 -0400

    added a few backgrounds

--- styles.js
+++ styles.js
@@ -2,21 +2,47 @@ var app = angular.module('myModule', []);
 app.controller('myController', function($scope, $interval){ 
 
 	function words(){
-	 	var word = ['what?\xA0', 'NO!\xA0', 'mine!\xA0', 'waaaaahhhhh!!!\xA0', 'gimme!\xA0'];
+	 	var word = [
+		 	'what?\xA0', 
+		 	'NO!\xA0', 
+		 	'mine!\xA0', 
+		 	'MINE!\xA0',
+		 	'waaaaahhhhh!!!\xA0', 
+		 	'gimme!\xA0',
+		 	'Steven Universe\xA0',
+		 	'why?\xA0',
+		 	'why?\xA0'
+	 	];
 	 	return word[Math.floor(Math.random() * word.length)];
 	};
-
+	function phrase(){
+	 	var phrases = [
+		 	'no fair!\xA0', 
+		 	'that\'s mine!\xA0', 
+		 	'she\'s hitting me!\xA0', 
+		 	'I\'m hungry!\xA0', 
+		 	'I don\'t wanna nap\xA0',
+		 	'can we get ice cream?\xA0',
+		 	'i want that!\xA0',
+		 	'how come?\xA0',
+		 	'nuh uh!\xA0',
+		 	'yeah huh!\xA0'
+	 	];
+	 	return phrases[Math.floor(Math.random() * phrases.length)];
+	};
 	function cssClass(){
 	 	var classes = [
 	 		'red',
 	 		'blue',
 	 		'green',
 	 		'big',
+	 		'reallybig',
 	 		'small',
 	 		'arial', 
 	 		'courier', 
 	 		'bold',
 	 		'yellow',
+	 		'cursive',
 	 		'aquamarine',
 	 		'blackback',
 	 		'violetback',
@@ -25,72 +51,47 @@ app.controller('myController', function($scope, $interval){
 	 	return classes[Math.floor(Math.random() * classes.length)];
 	};
 	
-	function phrase(){
-	 	var phrases = [
-	 	'no fair!\xA0', 
-	 	'thats mine!\xA0', 
-	 	'shes hitting me!\xA0', 
-	 	'Im hungry!\xA0', 
-	 	'I dont wanna nap\xA0' 
-	 	];
-	 	return phrases[Math.floor(Math.random() * phrases.length)];
-	};
-	function wordButton(){
+
+	function button(x){
 		var item = {
-			text: words(),
+			text: x,
 			style1: "",
 			style2: ""
 		};
 		return item;
 	};
-	function wordFancyButton(){
+	function fancyButton(x){
 		var item = {
-			text: words(),
+			text: x,
 			style1: cssClass(),
 			style2: cssClass()
 		};
 		return item;
 	};
-	function phraseButton(){
-		var item = {
-			text: phrase(),
-			style1: "",
-			style2: ""
-		};
-		return item;
-	};
-	function phraseFancyButton(){
-		var item = {
-			text: phrase(),
-			style1: cssClass(),
-			style2: cssClass()
-		};
-		return item;
-	};
+
 	function functions(){
 		var funct = [
-		wordButton(),
-		wordFancyButton(),
-		phraseButton(),
-		phraseFancyButton()
+		button(words()),
+		button(phrase()),
+		fancyButton(words()),
+		fancyButton(phrase())
 		];
 		return funct[Math.floor(Math.random() * funct.length)];
 	};
 
-
 	$scope.arr = [];
 
 	$scope.addWord= function(){
-		$scope.arr.push(wordButton());
+		$scope.arr.push(Button(words()));
 	};
 	$scope.addPhrase= function(){
-		$scope.arr.push(phraseButton());
+		$scope.arr.push(Button(phrase()));
 	};
 	$scope.addFancyStyle= function(){
-		$scope.arr.push(wordFancyButton());
+		$scope.arr.push(fancyButton(words()));
 	};
 	$scope.addFancyPhrase= function(){
-		$scope.arr.push(phraseFancyButton());
+		$scope.arr.push(fancyButton(phrase()));
 	};
 	$scope.bigButton= function(){	
 		$scope.arr.push(functions());
